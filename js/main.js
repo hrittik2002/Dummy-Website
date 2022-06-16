20f5a3478c0306f50ab0c1249d0c3879dd1324b9
Hrittik <hrittikgarain@gmail.com>
2022-06-16 21:37:27 +0530

    c7

--- js/main.js
+++ js/main.js
@@ -100,6 +100,8 @@
     
 })(jQuery);
 
+
+// pop up
 document.getElementById("contact-us-btn").addEventListener('click' , function(){
     document.querySelector('.bg-pop-up').style.display = 'flex';
     document.querySelector('.section-title').style.opacity = '0';
@@ -112,6 +114,43 @@ document.querySelector(".close-popup").addEventListener('click' , function(){
     document.querySelector('.bg-pop-up').style.display = 'none';
     document.querySelector('.section-title').style.opacity = '1';
     window.onscroll=function(){};
+})
+
+// sending email
+// const btn = document.getElementById('sendMessageButton');
+// btn.addEventListener('click' , function(e){
+//     e.preventDefault()
+//     const name = document.getElementById('pop-up-name').value;
+//     const email = document.getElementById('pop-up-email').value;
+//     const subject = document.getElementById('pop-up-subject').value;
+//     const message = document.getElementById('pop-up-message').value;
+//     const body = 'name :' + name + ' email : ' + email + ' subject : ' + subject + ' message : ' + message;  
+//     console.log(body);
+//     Email.send({
+//         Host : "smtp.yourisp.com",
+//         Username : "hrittikgarain@gmail.com",
+//         Password : "ikmvysavnfrwzflg",
+//         To : 'hrittikgarain@gmail.com',
+//         From : email,
+//         Subject : subject,
+//         Body : body
+//     }).then(
+//       message => alert(message)
+//     );
+// })
+
+const btn = document.getElementById('sendMessageButton');
+btn.addEventListener('click' , function(e){
+    e.preventDefault()
+    const params = {
+        from_name : document.getElementById('pop-up-name').value,
+        email_id : document.getElementById('pop-up-email').value,
+        message : document.getElementById('pop-up-message').value
+    }
+   
+   emailjs.send("service_i9hwmso" , "template_1z7wnc6" , params).then(function(res){
+    alert("Email Send");
+   })
 })
 
 
